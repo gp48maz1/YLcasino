dae7f672740922af2c5a308b1ae910f06888cd6e
phpuser <phpuser@MacBook-Pro.local>
2015-03-18 23:23:21 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+ 
+    <!--viewport goodness http://enva.to/A79s3G-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <title>YLYP Casino Fundraiser</title>
+     
+    <link rel="stylesheet" href="css/styles.css">
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+ 
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="http://webdesign.tutsplus.com/wp-content/themes/tuts/images/theme/webdesigntuts/favicon.ico" />
+    <link rel="apple-touch-icon" href="http://webdesign.tutsplus.com/wp-content/themes/tuts/images/theme/webdesigntuts/apple-touch-icon.png" />
+     
+</head>
+<body>
+<header class="band" data-0="background-position:0px 0px;" data-500="background-position: 0px -250px;">
+ 
+    <div class="container">
+
+        <div class="columns sixteen" data-0="opacity: 1" data-180="opacity: 0">
+            <h1>Young Life Casino Fundraiser</h1>
+ 
+            <a class="button" href="#">by YLYP</a>
+        </div><!--/ columns--> 
+     
+    </div><!--/ container-->       
+ 
+</header><!--/ band-->
+
+<div class="band main">
+ 
+    <div class="container">
+         
+        <div class="columns twelve offset-by-two">           
+                <h1>Date: April 11th 7:30</h1>
+                <h2>Location: 3512 Pleasant Valley Rd, Nashville TN 37204 (aka The Crib)</h2>
+                
+                 <p>LET'S SEND KIDS TO CAMP! All funds raised from the night will go straight towards sending kids to camp. There will be games, prizes, food, and you! </p>
+
+                 <p>Pre Order tickets are available at a <strong>Discounted Price </strong> of $10 for 20 chips or $35 for 100 chips. Please reach out to Gary Peters or David Ott for Pre-Order. 
+    
+        </div><!--/ column-->
+ 
+    </div><!--/ container-->
+ 
+</div><!--/ main--> 
+
+<div class="band main">
+ 
+    <div class="container">
+     
+        <hr>
+         
+        <div class="columns six offset-by-two">   
+                <h3>Want to pre-order Tickets?</h3>
+                
+                <p>Reach out to Gary Peters or David Ott.</p>
+        </div><!--/ column-->
+     
+        <div class="columns six">
+                <h3>Want to Get Involved in making this happen?</h3>
+                
+                <p>Reach out to Gary Peters or the YLYP: Nashville Facebook Page!</p>
+                     
+        </div><!--/ column-->
+ 
+    </div><!--/ container-->
+ 
+</div><!--/ main-->
+ 
+<footer class="band main">
+ 
+    <hr>
+ 
+    <div class="container">
+         
+        <div class="columns eight">
+                <p>Copyright &copy; Mazzone 2015</p>       
+                     
+        </div><!--/ column-->
+     
+        <div class="columns eight last">
+                <p>Instagram Twitter Facebook</p>
+         
+        </div><!--/ column-->
+ 
+    </div><!--/ container-->
+ 
+</footer><!--/ main-->
+</body>
+ <script type="text/javascript" src="js/skrollr.js"></script>
+ <script type="text/javascript">
+    skrollr.init({
+        smoothScrolling: true
+    });
+</script>
+</html>
